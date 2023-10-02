e904083388ffaded0b13fc4b1b2c9f2877dcd02e
Gerardo-Cabrera <gerardocabrera18@gmail.com>
2023-10-01 19:46:47 -0700

    Se agrega la carpeta con el front de la aplicacion

---
+++ react/src/components/TaskForm.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+
+function TaskForm() {
+  const [states, setStates] = useState([]);
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    date: '',
+    state: '',
+    creator: '',
+  });
+
+  useEffect(() => {
+    // Llamar a la API para obtener la lista de estados
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/states`)
+    .then((response) => response.json())
+    .then((data) => {
+        setStates(data);
+    })
+    .catch((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'Error al obtener la lista de estados'
+        });
+        console.error('Error al obtener la lista de estados: ', error);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          setFormData({
+            title: '',
+            description: '',
+            date: '',
+            state: '',
+            creator: '',
+          });
+          Swal.fire({
+            icon: 'success',
+            title: 'Tarea creada exitosamente.',
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 5000,
+          });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'ERROR',
+                text: 'Error al crear la tarea'
+            });
+          console.error('Error al crear la tarea.');
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'Error al crear la tarea'
+        });
+        console.error('Error al crear la tarea: ', error);
+      });
+  };
+
+  return (
+    <Container>
+      <div class="mt-3 mb-3">
+        <Link to="/" className="">Lista de Tareas</Link>
+      </div>
+      <h2 className="mt-3">Crear Nueva Tarea</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Título:</Form.Label>
+          <Form.Control
+            type="text"
+            value={formData.title}
+            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Descripción:</Form.Label>
+          <Form.Control
+            type="text"
+            value={formData.description}
+            onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Fecha:</Form.Label>
+          <Form.Control
+            type="date"
+            value={formData.date}
+            onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Estado:</Form.Label>
+          <Form.Control
+            as="select"
+            value={formData.state}
+            onChange={(e) => setFormData({ ...formData, state: e.target.value })}
+          >
+            <option value="">Selecciona un estado</option>
+            {states.map((state) => (
+              <option key={state.id} value={state.name}>
+                {state.name}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Creador:</Form.Label>
+          <Form.Control
+            type="text"
+            value={formData.creator}
+            onChange={(e) => setFormData({ ...formData, creator: e.target.value })}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Crear Tarea
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default TaskForm;
---
+++ react/src/components/TaskList.js
@@ -0,0 +1,308 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, InputGroup, FormControl, Button, ListGroup, Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+import io from 'socket.io-client';
+
+const socket = io.connect(process.env.REACT_APP_BACKEND_URL); 
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [states, setStates] = useState([]);
+  const [likes, setLikes] = useState(0);
+  const [likeButtonDisabled, setLikeButtonDisabled] = useState(false);
+  const [disabledLikeButtons, setDisabledLikeButtons] = useState(new Set());
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/tasks`)
+    .then((response) => response.json())
+    .then((data) => {
+        // Para cada tarea, obtener los likes y añadirlos como una propiedad "likes"
+        const tasksWithLikes = data.map(async (task) => {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/tasks/${task.id}/likes`);
+            const { likes } = await response.json();
+            return { ...task, likes };
+        });
+
+        // Esperar a que se resuelvan todas las promesas y establecer el estado
+        Promise.all(tasksWithLikes).then((tasksWithLikesData) => {
+            setTasks(tasksWithLikesData);
+            setFilteredTasks(tasksWithLikesData);
+        });
+    })
+    .catch((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'Error al obtener las tareas'
+        });
+        console.error('Error al obtener las tareas: ', error);
+    });
+
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/states`)
+    .then((response) => response.json())
+    .then((data) => {
+        setStates(data);
+    })
+    .catch((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'Error al obtener los estados'
+        });
+        console.error('Error al obtener la lista de estados: ', error);
+    });
+
+    socket.on('likeUpdated', (data) => {
+        const { taskId, count } = data;
+        // Actualizar el estado local con los likes actualizados
+        setLikes(data.count);
+        setTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === taskId ? { ...task, likes: count } : task
+          )
+        );
+      });
+  
+      // Limpieza del efecto al desmontar el componente
+      return () => {
+        socket.disconnect();
+      };
+  }, []);
+
+  const handleLike = (taskId) => {
+    if (!disabledLikeButtons.has(taskId)) {
+        setDisabledLikeButtons((prevSet) => new Set(prevSet.add(taskId)));
+
+        const updatedTasks = tasks.map((task) =>
+            task.id === taskId ? { ...task, likes: task.likes + 1 } : task
+        );
+        setTasks(updatedTasks);
+
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/tasks/${taskId}/likes`, {
+            method: 'POST',
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                // Emitir el evento al servidor para notificar sobre el like actualizado
+                socket.emit('likeUpdated', { taskId, count: data.count });
+                setLikes((prevLikes) => ({...prevLikes, [taskId]: data.count,}));
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'ERROR',
+                    text: 'Error al dar like a la tarea'
+                });
+                setTasks((prevTasks) =>
+                    prevTasks.map((task) =>
+                        task.id === taskId ? { ...task, likes: task.likes - 1 } : task
+                    )
+                );
+                setTasks(tasks);
+                setLikeButtonDisabled(false);
+            }
+        })
+        .catch((error) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'ERROR',
+                text: 'Error al dar like a la tarea'
+            });
+            console.error('Error al dar like a la tarea: ', error);
+            setTasks((prevTasks) =>
+                prevTasks.map((task) =>
+                    task.id === taskId ? { ...task, likes: task.likes - 1 } : task
+                )
+            );
+            setTasks(tasks);
+            setLikeButtonDisabled(false);
+        });
+    }
+  };
+
+  const handleEdit = (taskId) => {
+    Swal.fire({
+        title: 'Editar Likes',
+        input: 'text',
+        inputAttributes: {
+            autocapitalize: 'off',
+        },
+        inputValue: tasks.find(task => task.id === taskId).likes.toString(),
+        showCancelButton: true,
+        confirmButtonText: 'Guardar',
+        showLoaderOnConfirm: true,
+        preConfirm: (newLikes) => {
+            if (/^\d+$/.test(newLikes)) {
+                const newLikesInt = parseInt(newLikes, 10);
+
+                return fetch(`${process.env.REACT_APP_BACKEND_URL}/tasks/${taskId}/likes`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ count: newLikesInt }),
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data.success) {
+                        const updatedTasks = tasks.map((task) =>
+                            task.id === taskId ? { ...task, likes: newLikesInt } : task
+                        );
+                        setTasks(updatedTasks);
+                        Swal.fire('¡Actualizado!', 'El número de likes ha sido modificado.', 'success');
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'ERROR',
+                            text: 'Error al editar el conteo de likes'
+                        });
+                        throw new Error('Error al editar el conteo de likes.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error al editar el conteo de likes: ', error);
+                    Swal.showValidationMessage('Ocurrió un error al actualizar los likes.');
+                });
+            } else {
+                return 'Por favor, ingrese un número entero válido.';
+            }
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+    });
+  };
+
+  const handleDelete = (taskId) => {
+    const taskToDelete = tasks.find((task) => task.id === taskId);
+
+    if (taskToDelete.likes == 0 || taskToDelete.likes == null) {
+        Swal.fire({
+            title: '¿Estás seguro de eliminar esta tarea?',
+            text: 'Esta acción no se puede revertir.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sí, eliminar',
+            cancelButtonText: 'Cancelar',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`${process.env.REACT_APP_BACKEND_URL}/tasks/${taskId}`, {
+                    method: 'DELETE',
+                })
+                .then((response) => {
+                if (response.status === 200) {
+                    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+                    setTasks(updatedTasks);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'MENSAJE',
+                        text: 'Tarea eliminada'
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'ERROR',
+                        text: 'Error al eliminar la tarea'
+                    });
+                }
+                })
+                .catch((error) => {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'ERROR',
+                        text: 'Error al eliminar la tarea'
+                    });
+                    console.error('Error al eliminar la tarea: ', error);
+                });
+            }
+        });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'No se puede eliminar una tarea con likes'
+        });
+    }
+  };
+
+  const handleSearch = () => {
+    // Filtrar tareas por término de búsqueda
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTasks(filtered);
+  };
+
+  const handleFilter = (state) => {
+    // Filtrar tareas por estado
+    if (state === 'todos') {
+      setFilteredTasks(tasks);
+    } else {
+      const filtered = tasks.filter((task) => task.state === state);
+      setFilteredTasks(filtered);
+    }
+  };
+
+  return (
+    <Container className="mt-3">
+      <div class="mb-3">
+        <Link to="/crear-tarea" className="btn btn-success">Crear Tarea</Link>
+      </div>
+      <h1>Lista de Tareas</h1>
+      <InputGroup className="mb-3">
+        <FormControl 
+          placeholder="Buscar por título" 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button variant="outline-secondary" onClick={handleSearch}>Buscar</Button>
+      </InputGroup>
+      <div className="mb-3">
+        <Dropdown>
+          <Dropdown.Toggle variant="primary">
+            Filtrar por Estado
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => handleFilter('todos')}>Mostrar Todos</Dropdown.Item>
+                {states.map((state) => (
+                    <Dropdown.Item key={state.id} onClick={() => handleFilter(state.name)}>
+                        {state.name}
+                    </Dropdown.Item>
+                ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <ListGroup>
+        {filteredTasks.map((task) => (
+          <ListGroup.Item key={task.id} class="mt-3">
+            <h3>{task.title}</h3>
+            <p><b>Descripción:</b> {task.description}</p>
+            <p><b>Fecha:</b> {task.date}</p>
+            <p><b>Estado:</b> {task.state}</p>
+            <p><b>Creador:</b> {task.creator}</p>
+            {likes[task.id] !== undefined ? (
+                <p><b>Likes:</b> {likes[task.id]}</p>
+            ) : (
+                <p><b>Likes:</b> {task.likes}</p>
+            )}
+            <Button variant="success" className="me-2" onClick={() => handleLike(task.id)} disabled={disabledLikeButtons.has(task.id)}>Like</Button>
+            <Button variant="warning" className="me-2" onClick={() => handleEdit(task.id)}>Editar</Button>
+            <Button variant="danger" onClick={() => handleDelete(task.id)}>Eliminar</Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
+  );
+}
+
+export default TaskList;
